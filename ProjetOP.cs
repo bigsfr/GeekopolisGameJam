564f348089cbca1f581b45608807608d05d73f3b
bussiere <bussiere@gmail.com>
2013-05-11 14:18:27 +0200

    le papa pingouuuuuuuuuuuuiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiin

--- ProjetOP.cs
+++ ProjetOP.cs
@@ -19,14 +19,13 @@ namespace MonoGameTutorial
 
         // Enemies
 
-        List<Sprite> robots;
 
         // Compteur pour ajouter des enemies
         long spawnTime;
 
 
         // Fond du jeu
-        Texture2D backgroundSpace;
+        Texture2D backgroundSalon;
 
         // Fin du jeu ?
         bool finish;
@@ -51,16 +50,16 @@ namespace MonoGameTutorial
 			soundeffect.Play();
             Window.Title = "Projet OP";
 			
-			robots = new List<Sprite>();
+
         }
 
         protected override void Initialize()
         {
 			girl = new Sprite(this);
+
 			girl.Speed = new Vector2(2, 2);
 
-            robots.Clear();
-            spawnTime = 0;
+
             finish = false;
 			chambre_parent = false;
 			chambre_enfant = false;
@@ -79,7 +78,8 @@ namespace MonoGameTutorial
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // Fond de l'écran
-            backgroundSpace = Content.Load<Texture2D>("Salon");
+            backgroundSalon = Content.Load<Texture2D>("Salon");
+
 
             // Texture du vaisseau
 			girl.LoadContent("girl");
@@ -94,49 +94,17 @@ namespace MonoGameTutorial
             base.LoadContent();
         }
 
-        private void SpawnRobot()
-        {
-            // Nouveau robot
-            Sprite robot = new Sprite(this);
+      
 
-            // Suivant la valeur la valeur aléatoire on charge une texture plutôt qu'une autre
-            // Pour diversifier les enemies
-            Random rand = new Random();
-            if (rand.Next(3) % 2 == 0)
-                robot.LoadContent("robot/robotnormal");
-            else
-                robot.LoadContent("robot/spacealien");
 
-            // On lui attribut une position aléatoire sur X
-            // Pour ne pas qu'il sorte de l'écran à droite la valeur max de random doit être
-            // égale à la taille de l'écran - la largeur de la texture du robot
-            int posX = rand.Next(graphics.PreferredBackBufferWidth - robot.Width);
-            // Le robot n'est pas visible et descent du haut vers le bas
-            robot.Position = new Vector2(posX, -robot.Height);
-
-            // Plusieurs vitesse de défilement son possibles
-            if (rand.Next(6) % 5 == 0)
-                robot.Speed = new Vector2(0, 4);
-            else
-                robot.Speed = new Vector2(0, 2);
-
-            robots.Add(robot);
-        }
-
-        private void shoot()
-        {
-
-        }
 
         // Libération des ressources
         protected override void UnloadContent()
         {
-			backgroundSpace.Dispose();
+			backgroundSalon.Dispose();
             girl.UnloadContent();
 
 
-            foreach (Sprite robot in robots)
-                robot.UnloadContent();
 
             base.UnloadContent();
         }
@@ -147,48 +115,12 @@ namespace MonoGameTutorial
             if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 this.Exit();
 
-            #region Mise à jour des rectangles du vaisseau et du laser
+            #region Mise à jour des rectangles de la fille
             girl.Update(gameTime);
 
             #endregion
 
-            #region Mise à jour des enemies
-            // Toutes les 5 secondes on ajoute un robot
-            spawnTime += gameTime.ElapsedGameTime.Milliseconds;
-            if (spawnTime > 1000)
-            {
-                SpawnRobot();
-                spawnTime = 0;
-            }
 
-            // Pour chaque robot dans la collection
-            foreach (Sprite robot in robots)
-            {
-				// On met à jour le rectangle de chaque robot
-				robot.Update(gameTime);
-				
-                // Si le robot sort de l'écran on ne l'affiche plus et non le met plus à jour
-                if (robot.Position.Y >= graphics.PreferredBackBufferHeight)
-                    robot.Active = false;
-                else
-                    robot.Position = new Vector2(robot.Position.X, robot.Position.Y + robot.Speed.Y);
-				
-				#region Mise à jour des collisions
-				// Si un robot touche le vaisseau on arrete tout et on réinitialise l'écran
-                if (robot.Rectangle.Intersects(girl.Rectangle))
-                    finish = true;	
-				
-	
-				#endregion
-            }
-            #endregion
-
-            #region Mise à jour du laser du vaisseau
-
-            // Tire le laser
-            if (Keyboard.GetState().IsKeyDown(Keys.Space))
-                shoot();
-            #endregion
 			
             #region Mise à jour du vaisseau
             // Gestion de l'accélération du vaisseau
@@ -227,16 +159,14 @@ namespace MonoGameTutorial
             spriteBatch.Begin();
 
             // On affichage le fond à la position 0, 0
-            spriteBatch.Draw(backgroundSpace, Vector2.Zero, Color.White);
+            spriteBatch.Draw(backgroundSalon, Vector2.Zero, Color.White);
 
             // On affiche le vaisseau à la position définie dans Update()
             girl.Draw(spriteBatch);
 
 
 
-            // On dessine chaque robot actif
-            foreach (Sprite robot in robots)
-                robot.Draw(spriteBatch);
+
 
             // Fin du mode dessin
             spriteBatch.End();
